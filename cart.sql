9760540a5ef9dd98696779f430e3a5bf6073d72e
Mohamedfgfff <mohamed.your.mo.hisham012012@gmail.com>
2025-11-13 14:56:46 +0200

    ....

--- cart.sql
+++ cart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2025 at 01:42 PM
+-- Generation Time: Nov 13, 2025 at 01:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `cart` (
   `cart_product_title` varchar(255) NOT NULL,
   `cart_product_image` varchar(255) DEFAULT NULL,
   `cart_price` varchar(100) NOT NULL,
-  `cart_platform` varchar(100) NOT NULL
+  `cart_platform` varchar(100) NOT NULL,
+  `cart_tier` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
