c1d55dfa814614199ee33c74a45dba206dcf54e1
Mohamedfgfff <mh6285436@gmail.com>
2025-12-29 21:16:24 +0200

    feat: add endpoints to view service requests by service ID and user ID

--- local_services/service_request/view_by_service.php
+++ local_services/service_request/view_by_service.php
@@ -3,4 +3,9 @@ include "../../connect.php";
 
 $serviceid = filterRequest("serviceid");
 
+if ($serviceid == null) {
+    echo json_encode(array("status" => "fail", "message" => "serviceid is required"));
+    exit;
+}
+
 getAllData("service_requests", "service_id = $serviceid");
--- local_services/service_request/view_by_user.php
+++ local_services/service_request/view_by_user.php
@@ -3,4 +3,9 @@ include "../../connect.php";
 
 $userid = filterRequest("userid");
 
+if ($userid == null) {
+    echo json_encode(array("status" => "fail", "message" => "userid is required"));
+    exit;
+}
+
 getAllData("service_requests", "user_id = $userid");
